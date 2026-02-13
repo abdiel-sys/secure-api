e848e4e2d4bc5ff0572547c286d6aa4c414a4492
Abdiel Mendez <124847316+abdiel-sys@users.noreply.github.com>
2026-02-13 17:08:48 -0600

    registro terminado

--- app.py
+++ app.py
@@ -16,9 +16,29 @@ def register_user():
     try:
         user = UserSchema(**request.json)
     except ValidationError as e:
-        return jsonify({"error" : "Credenciales Invalidas"}), 400
-    return jsonify({"message" : "Validacion correcta"}), 201
+        return jsonify({"ERROR 400" : "Credenciales Invalidas"}), 400
 
+    try:
+        conn = sqlite3.connect('userdata.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT email FROM usuarios")
+
+        data = cursor.fetchall()
+
+        for row in data:
+            if (row[0] == user.email):
+                return jsonify({"ERROR 409": "El usuario ya existe"}), 409
+
+        bpassword = user.password.encode("utf-8")
+        salt = bcrypt.gensalt()
+        hash_password = bcrypt.hashpw(bpassword, salt)
+
+        cursor.execute("INSERT INTO usuarios (email, password) VALUES (?, ?)", (user.email, hash_password.decode()))
+        conn.commit()
+    except sqlite3.Error as error:
+        return jsonify({"ERROR 500" : "Error en el servidor"}), 500
+
+    return jsonify({"SUCCESS 201" : "Usuario Registrado"}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
